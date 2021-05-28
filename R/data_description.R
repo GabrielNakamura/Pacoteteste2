d84ecc00b8ab2a8ea50c1d9e8be9cb36e5afc12a
SeuNomeDeUsuario <seuMail@example.br>
2021-05-28 15:21:52 -0300

    updating package

---
+++ R/data_description.R
@@ -0,0 +1,19 @@
+#' Brincando com dados em pacote
+#'
+#' Exemplo de como colocar dados em pacotes, este dado pode ser qualquer objeto importado para
+#' dentro do pacote
+#'
+#' @format a matrix object with 5 rows and 2 columns
+#' @references \url{https://www.scielo.br/scielo.php?script=sci_arttext&pid=S1679-62252021000100211}
+#
+"data1"
+
+#' Brincando com dados em pacote
+#'
+#' Exemplo de como colocar dados em pacotes, este dado pode ser qualquer objeto importado para
+#' dentro do pacote
+#'
+#' @format a matrix object with 5 rows and 2 columns
+#' @references \url{https://www.scielo.br/scielo.php?script=sci_arttext&pid=S1679-62252021000100211}
+#
+"data2"
